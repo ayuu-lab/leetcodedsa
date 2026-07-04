a585b670b791a21d56143e2e2595b0b4eb1aa319
ayuu-lab <chitranshvarwade@gmail.com>
2026-07-04 20:27:00 +0530

    Time: 12 ms (6.94%) | Memory: 43.3 MB (28.44%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int sum = 0;
+        boolean isodd = false;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0 ; i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        System.out.println(map);
+        for(char ele : map.keySet()){
+            int freq = map.get(ele);
+            if(freq%2==0){ //even 
+                sum+=freq;
+            }else{
+                isodd = true;
+                sum+=(freq-1);
+            }
+        }
+        return isodd? sum+1:sum;
+    }
+}
