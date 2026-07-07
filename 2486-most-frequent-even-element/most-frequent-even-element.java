d9dde9e22be24ecb9f11717d2839328db8aa92d7
ayuu-lab <chitranshvarwade@gmail.com>
2026-07-07 12:37:23 +0530

    Time: 19 ms (46.52%) | Memory: 47.6 MB (62.75%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -7,7 +7,6 @@ class Solution {
                 map.put(i, map.getOrDefault(i, 0) + 1);
             }
         }
-        System.out.println(map);
         for (int i : map.keySet()) {
             if (map.get(i) >= max) { //
                 if (map.get(i) == max) { //
