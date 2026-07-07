d8623a52d8e130934542db5f8cee2f631b06487b
ayuu-lab <chitranshvarwade@gmail.com>
2026-07-07 12:37:04 +0530

    Time: 42 ms (12.51%) | Memory: 47.6 MB (62.75%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -1,24 +1,24 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int ans = Integer.MAX_VALUE , max = -1 ; 
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = Integer.MAX_VALUE, max = -1;
+        for (int i : nums) {
+            if (i % 2 == 0) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
+            }
         }
         System.out.println(map);
-        for(int i:map.keySet()){
-            if(i%2==0){ //check even 
-                if(map.get(i)>=max){ //
-                    if(map.get(i)==max){ //
-                        ans = Math.min(ans,i);
-                        max = map.get(i);
-                    }else{
-                        ans = i;
-                        max = map.get(i);
-                    }   
+        for (int i : map.keySet()) {
+            if (map.get(i) >= max) { //
+                if (map.get(i) == max) { //
+                    ans = Math.min(ans, i);
+                    max = map.get(i);
+                } else {
+                    ans = i;
+                    max = map.get(i);
                 }
             }
         }
-        return ans<Integer.MAX_VALUE? ans:-1;
+        return ans < Integer.MAX_VALUE ? ans : -1;
     }
 }
