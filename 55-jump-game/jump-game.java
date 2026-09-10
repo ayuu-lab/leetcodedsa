34c0b8a0440eec296e02b611e87ba85485bf720d
Chitransh Varwade <chitranshvarwade@gmail.com>
2026-09-10 19:38:34 +0530

    Time: 1 ms (99.94%) | Memory: 48.2 MB (8.62%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,25 +1,12 @@
 class Solution {
-    public boolean sol(int i, int arr[],Boolean dp[]) {
-        if (i >= arr.length)
-            return false;
-        if (i == arr.length - 1)
-            return true;
-        if(dp[i]!=null) return dp[i];
-        boolean ans = false;
-        for (int j = 1; j <= arr[i]; j++) {
-            ans = sol(j + i, arr,dp);
-            if (ans) {
-                break;
-            }
-
-        }
-        return dp[i]=ans;
-
-    }
-
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        Boolean dp[] = new Boolean[n+1];
-        return sol(0, nums,dp);
+        int l = nums.length;
+        int f = l-1;
+        for(int i=l-2;i>=0;i--){
+            if(i+nums[i]>=f){
+                f=i;
+            }
+        }
+        return f==0;
     }
 }
