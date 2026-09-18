fa517bd2fe58a78c84ec6ca645f7ac9ae8461115
Chitransh Varwade <chitranshvarwade@gmail.com>
2026-09-18 15:42:47 +0530

    Time: 321 ms (7.00%) | Memory: 48.3 MB (8.63%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,23 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int l = nums.length;
-        int f = l-1;
-        for(int i=l-2;i>=0;i--){
-            if(i+nums[i]>=f){
-                f=i;
+    public static boolean func(int []nums,int i, Boolean dp[] ){
+        if (i >= nums.length)
+            return false;
+        if (i == nums.length - 1)
+            return true;
+        if(dp[i]!=null) return dp[i];
+        boolean ans = false;
+        for(int j = 1 ; j<=nums[i];j++){
+            ans = func(nums,j+i,dp);
+            if(ans){
+                return ans;
             }
         }
-        return f==0;
+        return dp[i]=ans;
+    }
+
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        Boolean dp[] = new Boolean[len];
+        return func(nums , 0,dp);
     }
 }
