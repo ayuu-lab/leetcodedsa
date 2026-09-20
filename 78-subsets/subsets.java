9a0daaa0226370ddb2df0a50835913f9c641a38c
Chitransh Varwade <chitranshvarwade@gmail.com>
2026-09-20 23:03:23 +0530

    Time: 1 ms (87.13%) | Memory: 44.3 MB (20.63%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,20 +1,20 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        solve(ans,cur,nums,0);
-        return ans;
-    }
-    public void solve(List ans,List cur,int[] nums,int i){
+    public void func(int[] nums,List<List<Integer>> ans,List<Integer> temp,int i ) {
         if(i==nums.length){
-            ans.add(new ArrayList<>(cur));
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        //pick
-        cur.add(nums[i]);
-        solve(ans,cur,nums,i+1);
-        cur.remove(cur.size()-1);
-        //not pick
-        solve(ans,cur,nums,i+1);
+        temp.add(nums[i]);
+        func(nums,ans,temp,i+1);
+        temp.remove(temp.size()-1);
+        func(nums,ans,temp,i+1);
+        return;
+        
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        func(nums,ans,temp,0);
+        return ans;
     }
 }
